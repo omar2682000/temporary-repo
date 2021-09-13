7544d03194721298e6fbd2a28521b2a0093f7dc2
Omar Aljarrah <50204418+omar2682000@users.noreply.github.com>
2021-09-13 19:35:21 +0300

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ for index in range(len(text)):
     text[index] = text[index].replace("8080", "8090")
 server_config.close()
 
-server_config = open("/usr/share/tomcat9/conf/server.xml", "wt")
+server_config = open("/var/lib/tomcat7/conf/server.xml", "wt")
 server_config.writelines(text)
 server_config.close()
