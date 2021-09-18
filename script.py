72a066ad9aee64589ee1e8003afda877fa7b558c
Omar Aljarrah <50204418+omar2682000@users.noreply.github.com>
2021-09-18 04:18:20 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+# Configure tomcat port
 server_config = open("/var/lib/tomcat9/conf/server.xml", "rt")
 text = server_config.readlines()
 for index in range(len(text)):
@@ -7,3 +8,19 @@ server_config.close()
 server_config = open("/var/lib/tomcat9/conf/server.xml", "wt")
 server_config.writelines(text)
 server_config.close()
+
+
+# Configure tomcat users
+users_conf = """
+<role rolename="manager-gui"/>
+<role rolename="manager-script"/>
+<user username="admin" password="admin" roles="manager-gui,manager-script"/>
+"""
+tomcat_users = open("/var/lib/tomcat9/conf/tomcat-users.xml", "rt")
+text = server_config.readlines()
+text.insert(max(0, len(text) - 1), text = server_config.readlines())
+tomcat_users.close()
+
+tomcat_users = open("/var/lib/tomcat9/conf/tomcat-users.xml", "wt")
+tomcat_users.writelines(text)
+tomcat_users.close()
